2360363c71b870855f5a7447b097da7047333fb5
Шайхутдинов Амир <amchikshh@gmail.com>
2026-07-10 10:41:07 +0300

    сделал task 7 в новом модуле task7

---
+++ Task7/src/main/java/Task7/ru/itis/shop/infrastructure/persistence/jdbc/RowMapper.java
@@ -0,0 +1,8 @@
+package Task7.ru.itis.shop.infrastructure.persistence.jdbc;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public interface RowMapper<T> {
+    T mapRow(ResultSet row) throws SQLException;
+}
